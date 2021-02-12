6162320a0715e6202a26d67e4e0f459b66bf1504
bangddong <gusehdqkd@naver.com>
2021-02-12 20:46:46 +0900

    45일차  - 나도 쉬고싶다 ...

--- src/main/java/com/study/springboot/config/handler/CustomLoginSuccessHandler.java
+++ src/main/java/com/study/springboot/config/handler/CustomLoginSuccessHandler.java
@@ -13,7 +13,7 @@ public class CustomLoginSuccessHandler extends SavedRequestAwareAuthenticationSu
     /*
         AuthenticationProvider를 통해 인증이 성공될 경우 ㅊ처리 로직. /about로 리다이렉트
         나중에 사용자 정보를 꺼내 쓰기위해 Authentication 객체를 SecurityContextHolder Context에 저장한다.
-        꺼내쓸때도 해당 Context에서 꺼내쓰면 됨.  (세션방식)dd
+        꺼내쓸때도 해당 Context에서 꺼내쓰면 됨.  (세션방식)
      */
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
