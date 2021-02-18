d68bfe8555e0249b8c01db6f452193fc6ea2f176
bangddong <gusehdqkd@naver.com>
2021-02-18 23:03:01 +0900

    50일차  - 51일차까지 그냥 커밋용.

--- src/main/java/com/study/springboot/config/security/CustomAuthenticationFilter.java
+++ src/main/java/com/study/springboot/config/security/CustomAuthenticationFilter.java
@@ -21,6 +21,7 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
      * 로그인폼에서 넘어온 데이터 처리
      * 실제 로직은 CustomAuthenticationProvider에서 수행
      * 커밋용 주석
+     * 커밋용 주석2
      */
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
